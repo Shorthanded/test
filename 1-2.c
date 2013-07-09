f7d722a79e859bf61951d90e1704094da59717b4
Edwin Galloway <edwin.galloway@gmail.com>
2013-07-08 22:30:32 -0500

    minor style cleanup

--- 1-2.c
+++ 1-2.c
@@ -10,12 +10,12 @@
 // Fahrenheit is Natural
 // interp as the temp in degreees fahrenheit
 typedef double Fahrenheit;
-Fahrenheit F1 = -32;
-Fahrenheit F2 = 0;
-Fahrenheit F3 = 100.5;
-Fahrenheit BEGIN = 0;	// The amount at the beginning of the list
-Fahrenheit END   = 300; // The amount at the end of the list
-Fahrenheit STEP  = 20;  // The step for each output of the list
+const Fahrenheit F1 = -32;
+const Fahrenheit F2 = 0;
+const Fahrenheit F3 = 100.5;
+const Fahrenheit BEGIN = 0;	// The amount at the beginning of the list
+const Fahrenheit END   = 300; // The amount at the end of the list
+const Fahrenheit STEP  = 20;  // The step for each output of the list
 
 /*
 <Type> fn-for-fahrenheit(Fahrenheit f) {
@@ -28,12 +28,12 @@ Fahrenheit STEP  = 20;  // The step for each output of the list
 // Template rules used:
 // 	- atomic non-distinct: Natural
 
-// Fahrenheit is Celcius
+// Celcius is Natural
 // interp as the temp in degreees celcius
 typedef double Celcius;
-Celcius C1 = -32;
-Celcius C2 = 0;
-Celcius C3 = 100.5;
+const Celcius C1 = -32;
+const Celcius C2 = 0;
+const Celcius C3 = 100.5;
 
 /*
 <Type> fn-for-celcius(Celcius c) {
@@ -61,7 +61,7 @@ Celcius fahrenheit_to_celcius(Fahrenheit f) {
 // take a temperature in Celcius and convert it to Fahrenheit
 Fahrenheit celcius_to_fahrenheit(Celcius c) {
 	Fahrenheit f;
-	f = ((double)c / ( 5.0 / 9.0 )) + 32;
+	f = ( (double)c / ( 5.0 / 9.0 )) + 32;
 	return f;
 }
 
