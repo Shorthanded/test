c2c1a8bae6ea7d455e75de7116a11479aba3423b
Edwin Galloway <edwin.galloway@gmail.com>
2013-07-11 15:21:45 -0500

    1-5: first version

---
+++ 1-5.c
@@ -0,0 +1,22 @@
+/**********************************************************
+ * read a character
+ * 	while (character is not end-of-file token)
+ * 		output the character just read
+ * 		read a character
+ *********************************************************/
+
+#include <stdio.h>
+
+/*	copy input to output - 1st version	*/
+
+int main() {
+	int c;
+
+	c = getchar();
+	while (c != EOF) {
+		putchar( c );
+		c = getchar();
+	}
+
+	return 0;
+}
